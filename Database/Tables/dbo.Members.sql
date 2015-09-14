d50a5dfe91e486bd7e21b81b7e340140cad38af9
bcemmett <bcemmett@users.noreply.github.com>
2015-09-14 18:35:40 +0100

    Add Members table

---
+++ Database/Tables/dbo.Members.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[Members]
+(
+[MemberId] [int] NOT NULL IDENTITY(1, 1),
+[MembershipCode] [varchar] (20) COLLATE Latin1_General_CI_AS NOT NULL,
+[FirstName] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
+[LastName] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
+[Address1] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
+[Address2] [nvarchar] (30) COLLATE Latin1_General_CI_AS NULL,
+[PostCode] [nvarchar] (10) COLLATE Latin1_General_CI_AS NOT NULL,
+[City] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Members] ADD CONSTRAINT [PK_Members_MemberId] PRIMARY KEY CLUSTERED  ([MemberId]) ON [PRIMARY]
+GO
