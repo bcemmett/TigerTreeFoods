b4e0e78647f3c384a4b4bc8c77989ac0eea8f391
bcemmett <bcemmett@users.noreply.github.com>
2015-09-15 13:19:11 +0100

    Add some layout controls

--- PricingManager/PricingManager/MainForm.Designer.cs
+++ PricingManager/PricingManager/MainForm.Designer.cs
@@ -28,31 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControlPricing = new System.Windows.Forms.TabControl();
+            this.tabPageCurrentPricing = new System.Windows.Forms.TabPage();
+            this.buttonLoadCurrentPricingData = new System.Windows.Forms.Button();
+            this.buttonCancelAllOffers = new System.Windows.Forms.Button();
+            this.labelCurrentPricing = new System.Windows.Forms.Label();
+            this.tableLayoutPanelCurrentPricing = new System.Windows.Forms.TableLayoutPanel();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.labelFilter = new System.Windows.Forms.Label();
+            this.tabControlPricing.SuspendLayout();
+            this.tabPageCurrentPricing.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabControlPricing
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(781, 432);
-            this.tabControl1.TabIndex = 0;
+            this.tabControlPricing.Controls.Add(this.tabPageCurrentPricing);
+            this.tabControlPricing.Controls.Add(this.tabPage2);
+            this.tabControlPricing.Location = new System.Drawing.Point(12, 12);
+            this.tabControlPricing.Name = "tabControlPricing";
+            this.tabControlPricing.SelectedIndex = 0;
+            this.tabControlPricing.Size = new System.Drawing.Size(781, 432);
+            this.tabControlPricing.TabIndex = 0;
             // 
-            // tabPage1
+            // tabPageCurrentPricing
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(773, 406);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPageCurrentPricing.Controls.Add(this.labelFilter);
+            this.tabPageCurrentPricing.Controls.Add(this.textBoxFilter);
+            this.tabPageCurrentPricing.Controls.Add(this.buttonLoadCurrentPricingData);
+            this.tabPageCurrentPricing.Controls.Add(this.buttonCancelAllOffers);
+            this.tabPageCurrentPricing.Controls.Add(this.labelCurrentPricing);
+            this.tabPageCurrentPricing.Controls.Add(this.tableLayoutPanelCurrentPricing);
+            this.tabPageCurrentPricing.Location = new System.Drawing.Point(4, 22);
+            this.tabPageCurrentPricing.Name = "tabPageCurrentPricing";
+            this.tabPageCurrentPricing.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCurrentPricing.Size = new System.Drawing.Size(773, 406);
+            this.tabPageCurrentPricing.TabIndex = 0;
+            this.tabPageCurrentPricing.Text = "Current Pricing";
+            this.tabPageCurrentPricing.UseVisualStyleBackColor = true;
+            // 
+            // buttonLoadCurrentPricingData
+            // 
+            this.buttonLoadCurrentPricingData.BackColor = System.Drawing.Color.Transparent;
+            this.buttonLoadCurrentPricingData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLoadCurrentPricingData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoadCurrentPricingData.ForeColor = System.Drawing.Color.LimeGreen;
+            this.buttonLoadCurrentPricingData.Location = new System.Drawing.Point(497, 15);
+            this.buttonLoadCurrentPricingData.Name = "buttonLoadCurrentPricingData";
+            this.buttonLoadCurrentPricingData.Size = new System.Drawing.Size(132, 23);
+            this.buttonLoadCurrentPricingData.TabIndex = 3;
+            this.buttonLoadCurrentPricingData.Text = "Refresh data";
+            this.buttonLoadCurrentPricingData.UseVisualStyleBackColor = false;
+            // 
+            // buttonCancelAllOffers
+            // 
+            this.buttonCancelAllOffers.BackColor = System.Drawing.Color.Transparent;
+            this.buttonCancelAllOffers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCancelAllOffers.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancelAllOffers.ForeColor = System.Drawing.Color.Red;
+            this.buttonCancelAllOffers.Location = new System.Drawing.Point(635, 15);
+            this.buttonCancelAllOffers.Name = "buttonCancelAllOffers";
+            this.buttonCancelAllOffers.Size = new System.Drawing.Size(132, 23);
+            this.buttonCancelAllOffers.TabIndex = 2;
+            this.buttonCancelAllOffers.Text = "End all offers";
+            this.buttonCancelAllOffers.UseVisualStyleBackColor = false;
+            // 
+            // labelCurrentPricing
+            // 
+            this.labelCurrentPricing.AutoSize = true;
+            this.labelCurrentPricing.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentPricing.Location = new System.Drawing.Point(6, 12);
+            this.labelCurrentPricing.Name = "labelCurrentPricing";
+            this.labelCurrentPricing.Size = new System.Drawing.Size(150, 24);
+            this.labelCurrentPricing.TabIndex = 1;
+            this.labelCurrentPricing.Text = "Current Pricing";
+            // 
+            // tableLayoutPanelCurrentPricing
+            // 
+            this.tableLayoutPanelCurrentPricing.ColumnCount = 5;
+            this.tableLayoutPanelCurrentPricing.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 400F));
+            this.tableLayoutPanelCurrentPricing.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanelCurrentPricing.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanelCurrentPricing.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanelCurrentPricing.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanelCurrentPricing.Location = new System.Drawing.Point(6, 48);
+            this.tableLayoutPanelCurrentPricing.Name = "tableLayoutPanelCurrentPricing";
+            this.tableLayoutPanelCurrentPricing.RowCount = 1;
+            this.tableLayoutPanelCurrentPricing.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelCurrentPricing.Size = new System.Drawing.Size(761, 352);
+            this.tableLayoutPanelCurrentPricing.TabIndex = 0;
             // 
             // tabPage2
             // 
@@ -61,27 +125,52 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(773, 406);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Competitor Price Analysis";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(269, 15);
+            this.textBoxFilter.Multiline = true;
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(222, 23);
+            this.textBoxFilter.TabIndex = 4;
+            // 
+            // labelFilter
+            // 
+            this.labelFilter.AutoSize = true;
+            this.labelFilter.Location = new System.Drawing.Point(231, 20);
+            this.labelFilter.Name = "labelFilter";
+            this.labelFilter.Size = new System.Drawing.Size(32, 13);
+            this.labelFilter.TabIndex = 5;
+            this.labelFilter.Text = "Filter:";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(805, 456);
-            this.Controls.Add(this.tabControl1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
+            this.Controls.Add(this.tabControlPricing);
+            this.Name = "MainForm";
+            this.Text = "Pricing Manager";
+            this.tabControlPricing.ResumeLayout(false);
+            this.tabPageCurrentPricing.ResumeLayout(false);
+            this.tabPageCurrentPricing.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControlPricing;
+        private System.Windows.Forms.TabPage tabPageCurrentPricing;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelCurrentPricing;
+        private System.Windows.Forms.Button buttonCancelAllOffers;
+        private System.Windows.Forms.Label labelCurrentPricing;
+        private System.Windows.Forms.Button buttonLoadCurrentPricingData;
+        private System.Windows.Forms.Label labelFilter;
+        private System.Windows.Forms.TextBox textBoxFilter;
     }
 }
 
