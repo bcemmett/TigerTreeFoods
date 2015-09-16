ab4cd1cbb518e586bb6b52ee794868a1c876feaa
bcemmett <bcemmett@users.noreply.github.com>
2015-09-16 15:07:40 +0100

    Add random delay to competitor webservice

--- CompetitorPricingService/CompetitorPricingService/Controllers/ApiController.cs
+++ CompetitorPricingService/CompetitorPricingService/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 using System.Web.Mvc;
 
 namespace CompetitorPricingService.Controllers
@@ -14,6 +15,7 @@ namespace CompetitorPricingService.Controllers
             float competitorSaving = (float)random.Next(850, 1200) / 1000;
             Decimal basePrice = Decimal.Parse(ourPrice, NumberStyles.Currency);
             Decimal competitorPrice = (Decimal)competitorSaving * basePrice;
+            Thread.Sleep(random.Next(800, 2000));
             return Content(competitorPrice.ToString("C"));
         }
 	}
