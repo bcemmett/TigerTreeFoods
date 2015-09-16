4743f1556977882636b03c56517968b3727739fb
bcemmett <bcemmett@users.noreply.github.com>
2015-09-16 15:05:16 +0100

    Api method to return a competitor price

---
+++ CompetitorPricingService/CompetitorPricingService/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace CompetitorPricingService.Controllers
+{
+    public class ApiController : Controller
+    {
+        //
+        // GET: /Api/
+        public ActionResult GetPrice(string ourPrice, string description)
+        {
+            var random = new Random();
+            float competitorSaving = (float)random.Next(850, 1200) / 1000;
+            Decimal basePrice = Decimal.Parse(ourPrice, NumberStyles.Currency);
+            Decimal competitorPrice = (Decimal)competitorSaving * basePrice;
+            return Content(competitorPrice.ToString("C"));
+        }
+	}
+}
