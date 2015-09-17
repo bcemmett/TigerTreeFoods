eab3194e42e223c9c82f10c9c5e87bf8ab1a409d
bcemmett <bcemmett@users.noreply.github.com>
2015-09-17 23:13:56 +0100

    Show only members with recent transactions

--- LoyaltyProgramManagement/LoyaltyProgramManagement/TigerTreeDatabase.cs
+++ LoyaltyProgramManagement/LoyaltyProgramManagement/TigerTreeDatabase.cs
@@ -11,13 +11,23 @@ namespace LoyaltyProgramManagement
         {
             using (var db = new TigerTreeFoodsContext())
             {
-                IEnumerable<Member> members = db.Members.
-                    Where(m => 
+                IEnumerable<Member> members = db.Members
+                    .Where(m => 
                         (m.FirstName == firstName || firstName == null || firstName == String.Empty)
                         && (m.LastName == lastName || lastName == null || lastName == String.Empty)
                         && (m.City == city || city == null || city == String.Empty)
                         && (m.MembershipCode == membershipCode || membershipCode == null || membershipCode == String.Empty)
                     );
+                
+                if (recentTransactions)
+                {
+                    DateTime oneWeekAgo = DateTime.UtcNow.AddDays(-7);
+                    return members
+                        .Where(m => m.Transactions
+                            .Count(t => t.PurchaseDate > oneWeekAgo) > 0)
+                        .ToList();
+                }
+                
                 return members.Take(resultsToFetch).ToList();
             }
         }
