869679f259278d234a351d7068fe5d62ecc2699a
bcemmett <bcemmett@users.noreply.github.com>
2015-09-18 17:26:58 +0100

    Change db collation

--- Database/Schema/Tables/dbo.CurrentPrices.sql
+++ Database/Schema/Tables/dbo.CurrentPrices.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [dbo].[CurrentPrices]
 (
 [ItemId] [int] NOT NULL IDENTITY(1, 1),
-[Barcode] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
+[Barcode] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [RegularPrice] [money] NOT NULL,
 [OfferPrice] [money] NULL,
-[TillDescription] [nvarchar] (100) COLLATE Latin1_General_CI_AS NOT NULL,
+[TillDescription] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Image] [varbinary] (max) NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
--- Database/Schema/Tables/dbo.Members.sql
+++ Database/Schema/Tables/dbo.Members.sql
@@ -1,13 +1,13 @@
 CREATE TABLE [dbo].[Members]
 (
 [MemberId] [int] NOT NULL IDENTITY(1, 1),
-[MembershipCode] [varchar] (20) COLLATE Latin1_General_CI_AS NOT NULL,
-[FirstName] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
-[LastName] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
-[Address1] [nvarchar] (50) COLLATE Latin1_General_CI_AS NOT NULL,
-[Address2] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL,
-[PostCode] [nvarchar] (10) COLLATE Latin1_General_CI_AS NOT NULL,
-[City] [nvarchar] (30) COLLATE Latin1_General_CI_AS NOT NULL,
+[MembershipCode] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[FirstName] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[LastName] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Address1] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Address2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PostCode] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[City] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [FavouriteProduct] [int] NOT NULL
 ) ON [PRIMARY]
 CREATE UNIQUE NONCLUSTERED INDEX [NonClusteredIndex_MemberId_MembershipCode_FirstName_LastName_City_FavouriteProduct] ON [dbo].[Members] ([MemberId], [MembershipCode], [FirstName], [LastName], [City], [FavouriteProduct]) INCLUDE ([Address1], [Address2], [PostCode]) ON [PRIMARY]
