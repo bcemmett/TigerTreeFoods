850e27c0b847f6ca954c58035240a4a6041090b5
bcemmett <bcemmett@users.noreply.github.com>
2015-09-22 00:56:35 +0100

    Friendly names for search fields

--- LoyaltyProgramManagement/LoyaltyProgramManagement/Models/ViewModels/MembersTableModel.cs
+++ LoyaltyProgramManagement/LoyaltyProgramManagement/Models/ViewModels/MembersTableModel.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace LoyaltyProgramManagement.Models.ViewModels
 {
     public class MembersTableModel
     {
+        [DisplayName("First name")]
         public string FirstName { get; set; }
+        [DisplayName("Last name")]
         public string LastName { get; set; }
+        [DisplayName("Town / city")]
         public string City { get; set; }
+        [DisplayName("Recent transactions")]
         public bool RecentTransactions { get; set; }
+        [DisplayName("Membership code")]
         public string MembershipCode { get; set; }
+        [DisplayName("Results to display")]
         public int ResultsToDisplay { get; set; }
+        [DisplayName("Members")]
         public List<MemberModel> Members { get; set; }
     }
 }
--- LoyaltyProgramManagement/LoyaltyProgramManagement/Models/ViewModels/ProductsTableModel.cs
+++ LoyaltyProgramManagement/LoyaltyProgramManagement/Models/ViewModels/ProductsTableModel.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace LoyaltyProgramManagement.Models.ViewModels
 {
     public class ProductsTableModel
     {
+        [DisplayName("Description")]
         public string Description { get; set; }
+        [DisplayName("Barcode")]
         public string Barcode { get; set; }
+        [DisplayName("Products")]
         public List<ProductModel> Products { get; set; }
     }
 }
