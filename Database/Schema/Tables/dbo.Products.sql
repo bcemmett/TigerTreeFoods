346c5b51cddf316d2e557d2c2c2bc2373993ce40
bcemmett <bcemmett@users.noreply.github.com>
2015-10-04 18:12:04 +0100

    Rename pk contraint

--- Database/Schema/Tables/dbo.Products.sql
+++ Database/Schema/Tables/dbo.Products.sql
@@ -8,6 +8,8 @@ CREATE TABLE [dbo].[Products]
 [Image] [varbinary] (max) NULL,
 [Category] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+ALTER TABLE [dbo].[Products] ADD 
+CONSTRAINT [PK_Products_ProductId] PRIMARY KEY CLUSTERED  ([ProductId]) ON [PRIMARY]
 CREATE NONCLUSTERED INDEX [NonClusteredIndex_Category] ON [dbo].[Products] ([Category]) INCLUDE ([Barcode], [OfferPrice], [ProductId], [RegularPrice], [TillDescription]) ON [PRIMARY]
 
 CREATE UNIQUE NONCLUSTERED INDEX [NonClusteredIndex_ProductId_Barcode] ON [dbo].[Products] ([ProductId], [Barcode]) INCLUDE ([Category], [OfferPrice], [RegularPrice], [TillDescription]) ON [PRIMARY]
@@ -17,7 +19,6 @@ CREATE UNIQUE NONCLUSTERED INDEX [NonClusteredIndex_ProductId_Barcode] ON [dbo].
 
 
 GO
-ALTER TABLE [dbo].[Products] ADD CONSTRAINT [PK_CurrentPrices_ProductId] PRIMARY KEY CLUSTERED  ([ProductId]) ON [PRIMARY]
-GO
+
 ALTER TABLE [dbo].[Products] ADD CONSTRAINT [Unique_Barcode] UNIQUE NONCLUSTERED  ([Barcode]) ON [PRIMARY]
 GO
