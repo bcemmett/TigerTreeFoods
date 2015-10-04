bfd308ecb0c4223a4a42c0a01fcc09c69cbee8ed
bcemmett <bcemmett@users.noreply.github.com>
2015-10-04 14:08:55 +0100

    Add Category column to Products table

--- Database/Schema/Tables/dbo.Products.sql
+++ Database/Schema/Tables/dbo.Products.sql
@@ -5,12 +5,13 @@ CREATE TABLE [dbo].[Products]
 [RegularPrice] [money] NOT NULL,
 [OfferPrice] [money] NULL,
 [TillDescription] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Image] [varbinary] (max) NULL
+[Image] [varbinary] (max) NULL,
+[Category] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Products_Category] DEFAULT (N'Other')
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+CREATE UNIQUE NONCLUSTERED INDEX [NonClusteredIndex_ProductId_Barcode_Category] ON [dbo].[Products] ([ProductId], [Barcode], [Category]) INCLUDE ([OfferPrice], [RegularPrice], [TillDescription]) ON [PRIMARY]
+
 GO
 ALTER TABLE [dbo].[Products] ADD CONSTRAINT [PK_CurrentPrices_ProductId] PRIMARY KEY CLUSTERED  ([ProductId]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Products] ADD CONSTRAINT [Unique_Barcode] UNIQUE NONCLUSTERED  ([Barcode]) ON [PRIMARY]
-GO
-CREATE UNIQUE NONCLUSTERED INDEX [NonClusteredIndex_ProductId_Barcode] ON [dbo].[Products] ([ProductId], [Barcode]) INCLUDE ([OfferPrice], [RegularPrice], [TillDescription]) ON [PRIMARY]
 GO
