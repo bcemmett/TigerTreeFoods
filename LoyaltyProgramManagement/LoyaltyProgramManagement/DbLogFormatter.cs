1707d885dbde8057532f3c965df322c25509b5d5
bcemmett <bcemmett@users.noreply.github.com>
2015-10-06 19:30:26 +0100

    Add EF db logging formatter
    Note that this isn't included in the project by default!

---
+++ LoyaltyProgramManagement/LoyaltyProgramManagement/DbLogFormatter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace LoyaltyProgramManagement
+{
+    class SimpleLogDbConfiguration : DbConfiguration
+    {
+        public SimpleLogDbConfiguration()
+        {
+            SetDatabaseLogFormatter((context, writeAction) =>
+                new SimpleDatabaseLogFormatter(context, writeAction));
+        }
+    }
+
+    class SimpleDatabaseLogFormatter : DatabaseLogFormatter
+    {
+        public SimpleDatabaseLogFormatter(DbContext context, Action<string> writeAction)
+            : base(context, writeAction)
+        {
+        }
+        public override void LogCommand<TResult>(DbCommand command, DbCommandInterceptionContext<TResult> interceptionContext)
+        {
+            foreach (DbParameter p in command.Parameters)
+            {
+                Write(string.Format("DECLARE @{0} NVARCHAR(MAX) = '{1}'" + Environment.NewLine, p.ParameterName, p.Value));
+            }
+            Write(command.CommandText + Environment.NewLine + "GO" + Environment.NewLine + Environment.NewLine);
+        }
+
+        public override void LogResult<TResult>(DbCommand command, DbCommandInterceptionContext<TResult> interceptionContext)
+        {
+        }
+
+        public override void Opened(DbConnection connection, DbConnectionInterceptionContext interceptionContext)
+        {
+        }
+
+        public override void Closed(DbConnection connection, DbConnectionInterceptionContext interceptionContext)
+        {
+        }
+    }
+}
